e3ea96ecc4286ca5a043ec85b364dfb52ff0e3e4
brizzio <fabrizio.salvade@gmail.com>
2020-02-21 10:06:21 -0300

    update url image pesquisa

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ let card1 = {
     title: "Parametros de Pricing",
     img:"https://www.futureagenda.org/wp-content/uploads/2019/02/iStock_000003287087Medium-1280x800-c-center.jpg",
     panel_color:"indigo darken-1",
-    subtitle:"Nesta seção você pode definir as metas de faturamento, as margens minimas e máximas com as quais deseja trabalhar e quais produtos podem ou não seguir as indicações do sistema PROFIT MAIS ARTIFICIAL INTELLIGENCE",
+    subtitle:"Nesta seção você pode definir as metas de faturamento, as margens minimas e máximas com as quais deseja trabalhar e quais produtos podem ou não seguir as indicações do sistema PROFIT+ ARTIFICIAL INTELLIGENCE",
     links:[{title:"DEFINIR METAS",  go_to:"#"},{title:"PARAMETROS DE PRICING",  go_to:"#"}]
 }
 
 let card2 = {
     icon:"zoom_in",
     title: "Gestão de Pesquisas",
-    img:"https://cdn.business2community.com/wp-content/uploads/2019/10/How-To-Improve-Your-CPQ-Pricing-Strategies-image.jpg",
+    img:"https://apicms.thestar.com.my/uploads/images/2019/10/29/353925.png",
     panel_color:"deep-purple accent-2",
     subtitle:"Aqui você pode explorar os dados originados das pesquisas feitas junto aos concorrentes. Aqui voce encontra também um painel de controle para alterar o direcionamento dos nossos pesquisadores.",
     links:[{title:"DADOS DE PESQUISA",  go_to:"#"},{title:"AGENDAMENTO",  go_to:"#"},{title:"CONCORRENTES",  go_to:"#"}]
@@ -23,7 +23,7 @@ let card3 = {
     title: "Gestão de Preços",
     img:"https://www.supermarketnews.com/sites/supermarketnews.com/files/styles/article_featured_retina/public/Giant_Camp_Hill_Beer__Wine_Eatery_1_0.png?itok=YjELWc2P",
     panel_color:"pink darken-4",
-    subtitle:"Aqui você encontra a lista completa de produtos com o seu respectivo preço, resultado do sistema de pricing PROFIT MAIS. O painel permite a edição direta do preço final e exporta  para o ERP.",
+    subtitle:"Aqui você encontra a lista completa de produtos com o seu respectivo preço, resultado do sistema de pricing PROFIT+. O painel permite a edição direta do preço final e exporta  para o ERP.",
     links:[
         {title:"LISTA DE PREÇOS DE PRODUTOS",  go_to:"#"},
         {title:"DOWNLOAD DE ARQUIVOS",  go_to:"#"}
@@ -73,7 +73,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     <span class="card-title card-title-text">` + card.title + `</span>
                     </div>
                     <div class="card-content">
-                    <p>SELECIONE A OPÇÃO</p>
+                    <p>SELECIONE</p>
                     </div>
                     <div class="card-action">
                     ` + add_links(card.links) + `
